6d63d9d0f461926a987fe6e44d9e7a5c99b9ca27
futurecomputerscairns <44658054+futurecomputerscairns@users.noreply.github.com>
2019-03-14 14:04:46 +1000

    Update Get-GPPDriveMaps.ps1

--- Get-GPPDriveMaps.ps1
+++ Get-GPPDriveMaps.ps1
@@ -32,7 +32,7 @@ if($attempted_match.data[0].attributes.name -eq $organisation) {
 #Remove existing Flexible assets
 
 $existing = Get-ITGlueFlexibleAssets -filter_flexible_asset_type_id 120571 -filter_organization_id $ITGlueOrganisation
-$existing.data.id | % {Remove-ITGlueFlexibleAssets -id $_}
+$existing.data.id | % {Remove-ITGlueFlexibleAssets -id $_ -Confirm:$false}
 
 #Import the required module GroupPolicy
 $drivearray = @()
